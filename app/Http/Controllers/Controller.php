225fd64015bca2082566fc53d4f8028b3ba5c0e4
Jeferson Chagas Silva <jeferson_chagas25@hotmail.com>
2024-09-03 18:20:16 -0300

    feat: Criando documentação swagger para a api.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,22 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
+/**
+ * @OA\Info(
+ *    title="Projeto Login JWT",
+ *    version="1.0.0",
+ * ),
+ *  @OA\SecurityScheme(
+ *  type="http",
+ *  description="Token obtido na autenticação",
+ *  name="Authorization",
+ *  in="header",
+ *  scheme="bearer",
+ *  bearerFormat="JWT",
+ *  securityScheme="bearerToken"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
